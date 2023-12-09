766813086cadd14041119181eb42b2005bee9634
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2023-12-09 18:52:33 -0500

    Created enum CardCategoryColor for dynamic coloring of chart.

---
+++ src/model/Record/EcardCategoryColor.ts
@@ -0,0 +1,8 @@
+enum CardCategoryColor {
+    income = "rgba(53, 162, 235, 0.5)",
+    expenses = "rgba(255, 99, 132, 0.5)",
+    entertainment = "rgba(236, 167, 16, 0.5)",
+    fuel = "rgba(198, 200, 203, 1)"
+}
+
+export default CardCategoryColor;
