f2d931614780d382c9a6cda8212cca6158fd66b4
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2023-12-09 19:49:08 -0500

    Refactored distributionDetails to accept dynamic input

--- src/pages/details/distributionDetails/index.tsx
+++ src/pages/details/distributionDetails/index.tsx
@@ -1,4 +1,5 @@
 import CardCategory from "@/model/Record/EcardCategory"
+import CardCategoryColor from "@/model/Record/EcardCategoryColor"
 import IRecord from "@/model/Record/IRecord"
 import IRecordItem from "@/model/Record/IRecordItem"
 import { Pie } from "react-chartjs-2"
@@ -6,17 +7,22 @@ import { Pie } from "react-chartjs-2"
 
 type Props = {
     record: IRecord,
-    inputDate: Date
+    inputDate: Date,
+    choices: CardCategory[]
 }
 
-const DistributionDetails = ({ record, inputDate }: Props) => {
+const DistributionDetails = ({ record, inputDate, choices }: Props) => {
 
     const monthRecordsList = record.recordItemsList.filter(item => item.date.getFullYear() === inputDate.getFullYear() &&
         item.date.getMonth() === inputDate.getMonth());
 
-    const incomeItems = monthRecordsList.filter(item => item.category === CardCategory.INCOME)
-    const entertainmentItems = monthRecordsList.filter(item => item.category === CardCategory.ENTERTAINMENT)
-    const expensesItems = monthRecordsList.filter(item => item.category === CardCategory.EXPENSES)
+    // const incomeItems = monthRecordsList.filter(item => item.category === CardCategory.INCOME)
+    // const entertainmentItems = monthRecordsList.filter(item => item.category === CardCategory.ENTERTAINMENT)
+    // const expensesItems = monthRecordsList.filter(item => item.category === CardCategory.EXPENSES)
+
+    const filterItemsByCategory = (list: IRecordItem[], category: CardCategory) => {
+        return list.filter(item => item.category === category)
+    }
 
     const gettingTotalSum = (array: IRecordItem[]) => {
 
@@ -26,26 +32,22 @@ const DistributionDetails = ({ record, inputDate }: Props) => {
     }
 
     const data = {
-        labels: [CardCategory.INCOME, CardCategory.ENTERTAINMENT, CardCategory.EXPENSES],
+        labels: choices,
         datasets: [
             {
                 label: "% value",
-                data: [
-                    incomeItems.length > 0 ? gettingTotalSum(incomeItems) : 0,
-                    entertainmentItems.length > 0 ? gettingTotalSum(entertainmentItems) : 0,
-                    expensesItems.length > 0 ? gettingTotalSum(expensesItems) : 0
-                ],
-                backgroundColor: ["rgba(53, 162, 235, 0.5)",
-                    "rgba(236, 167, 16, 0.5)",
-                    "rgba(255, 99, 132, 0.5)"],
-                borderColor: ["rgba(53, 162, 235, 0.5)",
-                    "rgba(236, 167, 16, 0.5)",
-                    "rgba(255, 99, 132, 0.5)"],
+                data: choices.map(choice => {
+                    const list = filterItemsByCategory(monthRecordsList, choice);
+                    if (list.length > 0)
+                        return gettingTotalSum(list);
+                    return 0;
+                }),
+                backgroundColor: choices.map(choice => CardCategoryColor[choice]),
+                borderColor: choices.map(choice => CardCategoryColor[choice]),
                 borderWidth: 1
             }
         ]
     }
-
     return (
         <div>{
             monthRecordsList.length > 0 &&
