5d08802cd02ebe3d3f0c26b2ebc7833a855c145a
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2023-12-10 15:59:00 -0500

    Removed router in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,13 @@
-import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import Navbar from '@/shared/navbar'
-import CardDetails from '@/pages/details'
-import Login from '@/pages/auth/login'
 import Header from '@/shared/header'
-import About from '@/pages/about'
-import Home from '@/pages/home'
-import NewRecord from '@/pages/newFile/newRecord'
-import NewRecordItemForm from '@/pages/newFile/newRecordItemForm'
-import Error from '@/pages/error'
 import { getUserIdHook } from '@/customHooks/getUserIdHook';
+import router from '@/routes'
 
 function App() {
+
   const user = getUserIdHook();
-
-  const router = createBrowserRouter([
-    {
-      path: "/home",
-      element: <Home userId={user?.result?._id} />,
-    },
-    {
-      path: "/about",
-      element: <About />
-    },
-    {
-      path: "/newRecord",
-      element: <NewRecord />
-    },
-    {
-      path: "/home/:recordId/newDetails",
-      element: <NewRecordItemForm />
-    },
-    {
-      path: "/home/:recordId",
-      element: <CardDetails />,
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/error',
-      element: <Error userId={user?.result?._id} />
-    },
-    {
-      index: true,
-      element: <Navigate to="home" />
-
-    }
-  ])
-
-
-
-
 
   return (
     <div className="App">
