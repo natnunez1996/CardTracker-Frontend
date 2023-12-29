ba7ea55abff4ab3669290bee6bc44edc7fad8b83
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2023-12-28 19:32:29 -0500

    Refactor: Extracted Functions and Components outside RouterProvider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
-import Header from '@/shared/header'
-import { getUserIdHook } from '@/customHooks';
 import router from '@/routes'
-import { Box, PaletteMode, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
-import { useEffect, useMemo, useState } from 'react';
-
 
 function App() {
 
-  const user = getUserIdHook();
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const storedMode: string | null = localStorage.getItem('mode')
-
-  const [mode, setMode] = useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light')
-
-  const toggleColorMode = () => {
-    const newMode: 'light' | 'dark' = mode === 'light' ? 'dark' : 'light'
-    setMode(newMode)
-    localStorage.setItem('mode', newMode)
-  }
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode
-        },
-      }),
-    [mode]
-  );
-
-
-  useEffect(() => {
-    if (storedMode === 'light' || storedMode === 'dark') {
-      setMode(storedMode);
-    }
-  }, [storedMode])
-
   return (
-    <ThemeProvider theme={theme}>
-      <Header mode={mode} toggleColorMode={toggleColorMode} userProfile={user} />
-      <Box
-        minHeight={'93vh'}
-        sx={{
-          backgroundColor: theme.palette.mode === 'dark'
-            ? theme.palette.background.default
-            : theme.palette.background.paper,
-        }}
-      >
-        <RouterProvider router={router} />
-      </Box>
-    </ThemeProvider>
+    <RouterProvider router={router} />
   )
 }
 
