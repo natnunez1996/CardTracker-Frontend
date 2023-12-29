5c8cd5186ca7d0ee5f74e6c207018320a2427951
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2023-12-28 19:33:47 -0500

    Created: Root.tsx to be the parent component of RouterProvider.

---
+++ src/Root.tsx
@@ -0,0 +1,57 @@
+import { ThemeProvider } from "@emotion/react";
+import { useMediaQuery, PaletteMode, createTheme, Box } from "@mui/material";
+import { useState, useMemo, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { getUserIdHook } from "./customHooks";
+import Header from "./shared/header";
+
+const Root = () => {
+
+    const user = getUserIdHook();
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    const storedMode: string | null = localStorage.getItem('mode')
+
+    const [mode, setMode] = useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light')
+
+    const toggleColorMode = () => {
+        const newMode: 'light' | 'dark' = mode === 'light' ? 'dark' : 'light'
+        setMode(newMode)
+        localStorage.setItem('mode', newMode)
+    }
+
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode
+                },
+            }),
+        [mode]
+    );
+
+
+    useEffect(() => {
+        if (storedMode === 'light' || storedMode === 'dark') {
+            setMode(storedMode);
+        }
+    }, [storedMode])
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Header mode={mode} toggleColorMode={toggleColorMode} userProfile={user} />
+            <Box
+                minHeight={'93vh'}
+                sx={{
+                    backgroundColor: theme.palette.mode === 'dark'
+                        ? theme.palette.background.default
+                        : theme.palette.background.paper,
+                }}
+            >
+                <Outlet />
+            </Box>
+        </ThemeProvider>
+    )
+}
+
+export default Root;
