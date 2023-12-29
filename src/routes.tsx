28402890fddfe75c652c604844a5cec4a2542d84
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2023-12-28 19:33:01 -0500

    Refactor: Updated routes to include Header inside RouterProvider.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,44 +1,55 @@
 import { Navigate, createBrowserRouter } from "react-router-dom"
-import Home from "@/pages/home"
 import { getUserIdHook } from "@/customHooks";
-import NewRecord from "@/pages/newFile/newRecord";
-import NewRecordItemForm from "@/pages/newFile/newRecordItemForm";
+import AccountSettings from "@/pages/accountSettings";
 import CardDetails from "@/pages/details";
 import Error from '@/pages/error'
+import Home from "@/pages/home"
 import Login from "@/pages/auth/login";
+import NewRecord from "@/pages/newFile/newRecord";
+import NewRecordItemForm from "@/pages/newFile/newRecordItemForm";
+import Root from "./Root";
 
 
 const user = getUserIdHook();
 
 const router = createBrowserRouter([
   {
-    path: "/home",
-    element: <Home userId={user?.result?._id} />,
-  },
-  {
-    path: "/newRecord",
-    element: <NewRecord />
-  },
-  {
-    path: "/home/:recordId/newDetails",
-    element: <NewRecordItemForm />
-  },
-  {
-    path: "/home/:recordId",
-    element: <CardDetails />,
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/error',
-    element: <Error userId={user?.result?._id} />
-  },
-  {
-    index: true,
-    element: <Navigate to="home" />
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/accountSettings/:id',
+        element: <AccountSettings />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: "/home",
+        element: <Home userId={user?.result?._id} />,
+      },
+      {
+        path: "/home/:recordId",
+        element: <CardDetails />,
+      },
+      {
+        path: '/error',
+        element: <Error userId={user?.result?._id} />
+      },
+      {
+        path: "/home/:recordId/newDetails",
+        element: <NewRecordItemForm />
+      },
+      {
+        path: "/newRecord",
+        element: <NewRecord />
+      },
+      {
+        index: true,
+        element: <Navigate to="home" />
 
+      }]
   }
 ])
 
