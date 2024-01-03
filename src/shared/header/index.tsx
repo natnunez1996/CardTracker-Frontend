6c05a9c640f1b1ee0e8ad91d76cdf03e4049e34e
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-02 20:32:36 -0500

    Refactor: Different rendered buttons into a new component: Logout & Profile Buttons; added useMediaQuery hook.

--- src/shared/header/index.tsx
+++ src/shared/header/index.tsx
@@ -1,55 +1,32 @@
-import { AppBar, Avatar, Box, Button, IconButton, Menu, MenuItem, MenuList, Typography, useTheme } from "@mui/material";
-import { AccountCircleOutlined, LogoutOutlined } from "@mui/icons-material";
-import { IProfile } from "@/model/UserModel/IProfile";
-import { logout } from "@/actions/auth";
-import { useAppDispatch } from "@/hook";
-import { useEffect, useState } from "react";
+import { AppBar, Box, useMediaQuery, useTheme } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import ThemeSwitch from "../themeSwitch";
+import CardTrackerTitle from "@/common/CardTrackerTitle";
+import UserBar from "@/common/UserBar/UserBar";
+import { useEffect, useState } from "react";
+import { getUserIdHook } from "@/customHooks";
 import { IUser } from "@/model/UserModel";
-import CardLogo from '@/assets/img/CardTrackerLogo.png'
+import UserBarMobile from "@/common/UserBar/UserBarMobile";
+import ThemeSwitch from "@/common/ThemeSwitch";
 
 type Props = {
     mode: 'light' | 'dark',
     toggleColorMode: () => void,
-    userProfile: IProfile | undefined
 }
 
 
-const Header = ({ mode, toggleColorMode, userProfile }: Props) => {
-    const dispatch = useAppDispatch();
+const Header = ({ mode, toggleColorMode }: Props) => {
+    const match = useMediaQuery('(min-width: 600px');
     const navigate = useNavigate();
     const theme = useTheme();
-    const userData = localStorage.getItem('profile');
 
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>();
+    const userData = getUserIdHook()
     const [user, setUser] = useState<IUser>();
 
-    const open = Boolean(anchorEl)
-
-
-
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    }
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    }
-
     useEffect(() => {
-        if (userData !== null) {
-            const getUser = JSON.parse(userData)
-            setUser(getUser.result)
-        }
-
+        if (userData !== undefined)
+            setUser(userData.result)
     }, [])
 
-    const onLogOut = () => {
-        dispatch(logout);
-        navigate('/login', { replace: true })
-        window.location.reload()
-    }
 
 
 
@@ -57,48 +34,22 @@ const Header = ({ mode, toggleColorMode, userProfile }: Props) => {
         <AppBar sx={{
             backgroundColor: theme.palette.background.paper,
         }} position="sticky">
-            <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
-                <IconButton onClick={() => navigate('/home', { replace: true })} sx={{ color: theme.palette.text.primary, textDecoration: 'none' }}>
-                    <Avatar src={CardLogo} alt="CardTrackerLogo" sx={{ width: 40, height: 40, borderRadius: 0 }} />
-                    <Typography variant="h4">Card Tracker</Typography>
-                </IconButton>
-                {!userProfile && <div className="middle"> <h1>Please <a href="/login">Login</a> first</h1></div>}
-                <Box display={'flex'} justifyContent={'space-evenly'} alignItems={'center'}>
-                    {
-                        user &&
-                        <Button onClick={handleClick}>
-                            <Typography variant="h6" sx={{ color: theme.palette.text.primary }}>{`${user.firstName} ${user.lastName}`}</Typography>
-                        </Button>
-                    }
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        onClick={handleClose}
-                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                    >
-                        <MenuList>
-                            <MenuItem>
-                                <IconButton
-                                    onClick={() => navigate(`/accountSettings/${user?._id}`)}
-                                    sx={{ color: theme.palette.text.primary }}
-                                >
-                                    <AccountCircleOutlined />
-                                    <Typography variant="button">Profile</Typography>
-                                </IconButton>
-                            </MenuItem>
-                            <MenuItem>
-                                <IconButton onClick={onLogOut}>
-                                    <LogoutOutlined color="error" />
-                                    <Typography variant="button" sx={{ color: theme.palette.error.main }}>Log Out</Typography>
-                                </IconButton>
-                            </MenuItem>
-                        </MenuList>
-                    </Menu>
-                    <ThemeSwitch mode={mode} handleClick={toggleColorMode} theme={theme} />
-                </Box>
-            </Box>
+            {user &&
+                (match ?
+                    //UI for PC
+                    <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                        <CardTrackerTitle navigate={navigate} theme={theme} />
+                        <UserBar mode={mode} navigate={navigate} theme={theme} toggleColorMode={toggleColorMode} user={user} />
+                    </Box>
+                    :
+                    //UI for Mobile Devices
+                    <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+                        <UserBarMobile navigate={navigate} theme={theme} user={user} />
+                        <CardTrackerTitle navigate={navigate} theme={theme} />
+                        <ThemeSwitch mode={mode} handleClick={toggleColorMode} theme={theme} />
+                    </Box>
+                )
+            }
         </AppBar >
     )
 }
