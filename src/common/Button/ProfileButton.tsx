89885bf177dbc73bbcc326c17ad8420b05e64755
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-02 20:30:20 -0500

    Create: Profile Button

---
+++ src/common/Button/ProfileButton.tsx
@@ -0,0 +1,25 @@
+import { IUser } from "@/model/UserModel"
+import { AccountCircleOutlined } from "@mui/icons-material"
+import { IconButton, ListItemIcon, Theme, Typography } from "@mui/material"
+import { NavigateFunction } from "react-router-dom"
+
+type Props = {
+    buttonName?: string,
+    navigate: NavigateFunction,
+    theme: Theme,
+    user: IUser
+}
+
+export const ProfileButton = ({ buttonName = 'Profile', navigate, theme, user }: Props) => {
+    return (
+        <IconButton
+            onClick={() => navigate(`/accountSettings/${user?._id}`)}
+            sx={{ color: theme.palette.text.primary }}
+        >
+            <ListItemIcon>
+                <AccountCircleOutlined />
+            </ListItemIcon>
+            <Typography variant="button">{buttonName}</Typography>
+        </IconButton>
+    )
+}
