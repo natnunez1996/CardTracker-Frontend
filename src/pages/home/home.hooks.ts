c946b95aa551576519c717a91225ea5ec83d745e
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-03 19:49:37 -0500

    Added: home.hook.ts that contains all the useEffect of HomePage.

---
+++ src/pages/home/home.hooks.ts
@@ -0,0 +1,36 @@
+import { getAllRecordsOfUser } from "@/actions/record"
+import { useAppDispatch } from "@/hook"
+import { IRecord } from "@/model/CardModel"
+import { useEffect } from "react"
+import { NavigateFunction } from "react-router-dom"
+
+type Props = {
+    navigate: NavigateFunction,
+    records: any,
+    reload: boolean,
+    setUserRecords: React.Dispatch<React.SetStateAction<IRecord[] | undefined>>,
+    userId: string | null
+}
+
+export const useHomePage = ({ navigate, records, reload, setUserRecords, userId }: Props) => {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        //Navigate to Log In if there is no user logged in.
+        if (userId === undefined) {
+            navigate('/login', { replace: true })
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.removeItem("lastKnownInputDate")
+
+        if (userId)
+            dispatch(getAllRecordsOfUser(userId))
+    }, [userId, reload])
+
+    useEffect(() => {
+
+        setUserRecords(records.records);
+    }, [records])
+}
