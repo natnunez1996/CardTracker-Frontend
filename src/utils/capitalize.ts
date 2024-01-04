0d9a72be6d80f41113990a98b1f30627f49950da
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-03 19:52:12 -0500

    Added: capitalize utils.

---
+++ src/utils/capitalize.ts
@@ -0,0 +1,25 @@
+export const capitalize = (input: string): string => {
+    let output = '';
+
+    let newInput = input.split(' ').filter(word => word !== undefined);
+
+    if (newInput.length > 1) {
+        newInput.forEach(word => {
+            if (isLetter(word[0]))
+                output += word[0].toUpperCase() + word.slice(1) + ' ';
+            else
+                output += word + ' ';
+        });
+    } else if (newInput.length === 1) {
+        output += newInput[0][0].toUpperCase() + newInput[0].slice(1);
+    }
+
+    return output.trim();
+};
+
+function isLetter(input: string): boolean {
+    if (input && input.match(/^[A-Za-z]+$/)) {
+        return true;
+    }
+    return false;
+}
