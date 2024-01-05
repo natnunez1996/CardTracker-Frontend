7cd59ed6098b41161433e72f52c0dfbc72782bc9
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-04 19:29:59 -0500

    Refactor: Created a responsive UI

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,16 @@
 import { useAppDispatch, useAppSelector } from '@/hook';
-import { useEffect, useState } from 'react';
-import { deleteRecord, getAllRecordsOfUser } from '@/actions/record';
+import { useState } from 'react';
+import { deleteRecord } from '@/actions/record';
 import { useNavigate } from 'react-router-dom';
-import CardItem from '@/common/CardItem';
-import { Paper, IconButton, Popper, Box, Button, Grid, useTheme, Typography } from '@mui/material';
+import { Typography, useTheme } from '@mui/material';
 import { IRecord } from '@/model/CardModel';
-import { CreditCardTwoTone } from '@mui/icons-material';
-import { calculateBalance } from '@/utils';
 import { getMediaMatch } from '@/customHooks';
 import { useHomePage } from './home.hooks';
+import HomePage from './Home/HomePage';
+import HomePageMobile from './Home/HomePageMobile';
 
 type Props = {
-    userId: string | null;
+    userId: string;
 }
 
 const Home = ({ userId }: Props) => {
@@ -21,9 +20,7 @@ const Home = ({ userId }: Props) => {
     const theme = useTheme()
     const records = useAppSelector(state => state.userRecords);
 
-    const [userRecords, setUserRecords] = useState<IRecord[]>();
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
+    const [userRecords, setUserRecords] = useState<IRecord[] | null>(null);
     const [recordIdToDelete, setRecordIdToDelete] = useState<string | null>(null);
     const [reload, setReload] = useState(true);
 
@@ -39,61 +36,31 @@ const Home = ({ userId }: Props) => {
 
 
     return (
-        <Box
-            display={'flex'}
-            flexDirection={'column'}
-            width={'100%'}
-            sx={{
-                backgroundColor: theme.palette.mode === 'dark'
-                    ? theme.palette.background.default
-                    : theme.palette.background.paper,
-            }}
-        >
-            {userId &&
-                <>
-                    <Paper elevation={3} sx={{ height: 50, alignSelf: 'center', margin: '1rem' }}>
-                        <IconButton color='primary' onClick={() => navigate('/newRecord', { replace: true })}>
-                            Add Card <CreditCardTwoTone />
-                        </IconButton>
-                    </Paper>
-                    {userRecords && userRecords.length > 0 ?
-                        <Grid
-                            container spacing={2}
-                            justifyContent={'flex-start'}
-                            alignSelf={'center'}
-                        >
-                            {userRecords.sort((a: IRecord, b: IRecord) => {
-                                const dateA = new Date(a.updatedDate);
-                                const dateB = new Date(b.updatedDate);
-                                return dateB.getTime() - dateA.getTime();
-                            })
-                                .map((record) => (
-                                    <Grid item key={record._id?.toString()}  >
-                                        <CardItem
-                                            anchorEl={anchorEl}
-                                            calculateBalance={calculateBalance}
-                                            navigate={navigate}
-                                            record={record}
-                                            setAnchorEl={setAnchorEl}
-                                            setRecordIdToDelete={setRecordIdToDelete}
-                                        />
-                                    </Grid>
-                                ))}
-                        </Grid>
+        <>
+            {userRecords && userRecords.length > 0 ?
+                (
+                    match ?
+                        <HomePage
+                            handleDelete={handleDelete}
+                            records={records}
+                            setRecordIdToDelete={setRecordIdToDelete}
+                            theme={theme}
+                            userRecords={userRecords}
+                        />
                         :
-                        <Typography variant='h2' sx={{ color: 'red' }} alignSelf={'center'}>
-                            {records.message}
-                        </Typography>}
-
-                    <Popper open={open} anchorEl={anchorEl}>
-                        <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }} >
-                            <Button color='error' onClick={handleDelete}> DELETE </Button>
-                            <Button color='success' onClick={() => setAnchorEl(null)}> Cancel </Button>
-                        </Box>
-                    </Popper>
-                </>
+                        <HomePageMobile
+                            handleDelete={handleDelete}
+                            records={records}
+                            setRecordIdToDelete={setRecordIdToDelete}
+                            theme={theme}
+                            userRecords={userRecords}
+                        />)
+                :
+                <Typography variant='h2' sx={{ color: 'red' }} alignSelf={'center'}>
+                    {records.message}
+                </Typography>
             }
-        </Box >
+        </>
     )
 }
 
