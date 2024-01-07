e68079a62ac04d68d7854ad2c0f6da55869dc65f
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-06 19:58:15 -0500

    Created: details.hooks.ts that contains all useEffect of details page.

---
+++ src/pages/details/details.hooks.ts
@@ -0,0 +1,71 @@
+import { updateRecord } from "@/actions/record";
+import { CardCategory } from "@/enums/ECard";
+import { useAppDispatch } from "@/hook";
+import { IRecord, IRecordItem } from "@/model/CardModel";
+import { IProfile } from "@/model/UserModel";
+import { filterRecordsListByDate } from "@/utils";
+import { SetStateAction, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const useDetailsPage = (
+    cardDetails: IRecord | undefined,
+    inputDate: Date,
+    updateDetails: IRecord | undefined,
+    user: IProfile | undefined,
+    setAmountEarnLoss: React.Dispatch<SetStateAction<number>>,
+    setInputDate: React.Dispatch<SetStateAction<Date>>,
+    setInputDateRecordList: React.Dispatch<SetStateAction<IRecordItem[] | never>>,
+    setUpdateDetails: React.Dispatch<SetStateAction<IRecord | undefined>>,
+    storedDate: string | null,
+    toDelete: boolean,
+) => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user)
+            navigate('/home', { replace: true });
+
+        if (storedDate)
+            setInputDate(new Date(storedDate))
+
+        if (cardDetails !== undefined && cardDetails.recordItemsList) {
+            if (cardDetails.recordItemsList.length > 0) {
+                localStorage.setItem("lastKnownId",
+                    cardDetails.recordItemsList[cardDetails.recordItemsList.length - 1].id.toString())
+            }
+
+            if (cardDetails.recordItemsList.length === 0) {
+                localStorage.setItem("lastKnownId", "0");
+            }
+
+            setUpdateDetails(cardDetails);
+        }
+    }, [cardDetails])
+
+    useEffect(() => {
+        if (updateDetails) {
+            dispatch(updateRecord(updateDetails, navigate))
+            navigate(0)
+        }
+    }, [toDelete])
+
+    useEffect(() => {
+        localStorage.setItem("lastKnownInputDate", inputDate.toDateString())
+
+        if (updateDetails !== undefined) {
+            const filteredDetails = filterRecordsListByDate(updateDetails.recordItemsList, inputDate)
+            setInputDateRecordList(filteredDetails)
+
+            setAmountEarnLoss(0);
+            filteredDetails.forEach(recordItem => {
+                if (recordItem.category === CardCategory.INCOME)
+                    setAmountEarnLoss(prevAmount => prevAmount += recordItem.amount);
+                else
+                    setAmountEarnLoss(prevAmount => prevAmount -= recordItem.amount);
+
+            })
+        }
+    }, [inputDate])
+
+}
