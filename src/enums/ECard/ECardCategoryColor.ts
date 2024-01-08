c33847c5ecb1235529fa6285d5b8b28c01779c1f
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-07 19:10:21 -0500

    Refactor: Changed the colors of all CardCategoryColor

--- src/enums/ECard/ECardCategoryColor.ts
+++ src/enums/ECard/ECardCategoryColor.ts
@@ -1,6 +1,6 @@
 export enum CardCategoryColor {
-    income = "rgba(53, 162, 235, 0.5)",
-    expenses = "rgba(255, 99, 132, 0.5)",
-    entertainment = "rgba(236, 167, 16, 0.5)",
-    fuel = "rgba(198, 200, 203, 1)"
+    income = "rgba(0, 0, 255)",
+    expenses = "rgba(255, 0, 51)",
+    entertainment = "rgba(0, 255, 102)",
+    fuel = "rgba(153, 0, 153)"
 }
