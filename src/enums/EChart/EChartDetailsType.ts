5cd2fb4bfa83b901fb422a25ea03e578f3a811cd
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-01-07 19:10:50 -0500

    Added: EChartDetailsType for Mobile version of Details Component

---
+++ src/enums/EChart/EChartDetailsType.ts
@@ -0,0 +1,8 @@
+enum ChartDetailsType {
+    DEFAULT = "all",
+    DISTRIBUTION_DETAILS = 'distribution',
+    EXPENSES_DETAILS = "expensesDetails",
+    MONTHLY_DETAILS = "monthlyDetails"
+}
+
+export default ChartDetailsType
