7ab7e398aacc8e4604f4ba9e8346b8621096cade
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-06-11 18:38:34 -0400

    Refactor: Added notice for the backend issue.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import router from '@/routes'
+import { useEffect } from 'react'
 
 const App = () => {
+
+  useEffect(() => {
+    alert("Hi! Nathaniel here. I am sorry to inform you that this application is not working because of the discontinuation of cyclic.sh which is responsible for hosting the server of this app. I will update as soon as I find another free hosting website. Thank you for understanding.")
+  }, [])
+
   return (
     <RouterProvider router={router} />
   )
