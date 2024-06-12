08f6795620d20b953b07491b2325439c050b4fdc
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2024-06-12 17:24:16 -0400

    Refactor: Changed import path

--- src/router.tsx
+++ src/router.tsx
@@ -5,9 +5,9 @@ import AccountSettings from './pages/AccountSettings/AccountSettings'
 import NewRecord from './pages/NewFile/NewRecord'
 import NewRecordItemForm from './pages/NewFile/NewRecordItemForm'
 import ErrorPage from './pages/Error/ErrorPage';
-import LoginPage from '@/pages/auth/LoginPage'
-import HomePage from '@/pages/home/HomePage'
-import DetailsPage from '@/pages/details/DetailsPage'
+import LoginPage from '@/pages/Auth/LoginPage'
+import HomePage from '@/pages/Home/HomePage'
+import DetailsPage from './pages/Details/DetailsPage'
 
 const user = getUserIdHook()
 
